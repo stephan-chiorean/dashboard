cb83e2f52b1b52dc2dbefceaf26dc31ffc06cf63
stephan-chiorean <112440764+stephan-chiorean@users.noreply.github.com>
2023-04-04 00:33:51 -0700

    beginning build

---
+++ server/controllers/client.js
@@ -0,0 +1,82 @@
+import Product from '../models/Product.js';
+import ProductStat from '../models/ProductStat.js';
+import User from '../models/User.js';
+import Transaction from '../models/Transaction.js';
+
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    const productsWithStats = await Promise.all(
+      products.map(async (product) => {
+        const stat = await ProductStat.find({
+          productId: product._id,
+        });
+        return {
+          ...product._doc,
+          stat,
+        };
+      })
+    );
+    res.status(200).json(productsWithStats);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const getCustomers = async (req, res) => {
+  try {
+    const customers = await User.find({ role: 'user' }).select('-password');
+
+    res.status(200).json(customers);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const getTransactions = async(req, res) => {
+  try {
+    //sort object looks like this: {"field": "userId", "sort": "desc"}
+    const {page=1, pageSize = 20, sort = null, search = ""} = req.query;
+
+    console.log("Here it is", [page, pageSize, sort, search]);
+
+    //mongo expects {userId: -1}
+    const generateSort = () => {
+      const sortParsed = JSON.parse(sort);
+      const sortFormatted = {
+        [sortParsed.field]: sortParsed.sort = "asc" ? 1 : -1
+      }
+
+      return sortFormatted;
+    }
+    const sortFormatted = Boolean(sort) ? generateSort() : {};
+
+    const transactions = await Transaction.find({
+      $or: [
+        {cost: {$regex: new RegExp(search, "i")}},
+        {userId: {$regex: new RegExp(search, "i")}}
+      ],
+    })
+    .sort(sortFormatted)
+    .skip(page * pageSize)
+    .limit(pageSize);
+    // const totally = 500
+    // const tryTotal = await Transaction.countDocuments({})
+    const total = await Transaction.countDocuments(
+      search === "" ? {} : {
+        $or: [
+          { name: { $regex: new RegExp(search, "i") } },
+          { userId: { $regex: new RegExp(search, "i") } }
+        ]
+      }
+    );
+
+    res.status(200).json({
+      transactions,
+      total,
+    })
+  } catch(err) {
+    res.status(404).json({message: err.message})
+  }
+}
