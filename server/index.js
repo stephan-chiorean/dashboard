f87cbd03a53e5157e422cf6a7b3ee150d7bce7f4
Stephan Chiorean <stephanchiorean@Mac.hsd1.wa.comcast.net>
2024-12-25 11:38:30 -0800

    get app running

--- server/index.js
+++ server/index.js
@@ -1,39 +1,46 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import clientRoutes from './routes/client.js';
-import generalRoutes from './routes/general.js';
-import managementRoutes from './routes/management.js';
-import salesRoutes from './routes/sales.js';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
 
-import User from "./models/User.js"
-import Product from './models/Product.js';
-import ProductStat from './models/ProductStat.js';
-import Transaction from './models/Transaction.js';
-import OverallStat from './models/OversallStat.js';
-import AffiliateStat from './models/AffiliateStat.js';
-import {dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat, dataAffiliateStat} from "./data/index.js"
+import User from "./models/User.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import Transaction from "./models/Transaction.js";
+import OverallStat from "./models/OversallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+  dataOverallStat,
+  dataAffiliateStat,
+} from "./data/index.js";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
-app.use(morgan('common'));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use('/client', clientRoutes);
-app.use('/general', generalRoutes);
-app.use('/management', managementRoutes);
-app.use('/sales', salesRoutes);
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 5001;
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
